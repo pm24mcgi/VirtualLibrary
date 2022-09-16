3a5f20c000e8341ed604a584536eedbe73c28dff
pm24mcgi <patrick.mcginn@troyweb.com>
2022-09-16 10:42:28 -0400

    Program.cs comments

--- VirtualLibrary/Program.cs
+++ VirtualLibrary/Program.cs
@@ -1,10 +1,18 @@
+// Assuming these are all imports?
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using VirtualLibrary.Data;
 
+// Builder has configuration, logging, and many other services added to the DI container
 var builder = WebApplication.CreateBuilder(args);
 
+//Establishes the connection string pulled from appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add SERVICES to the container.
+// Services are typically resolved from DI using constructor injection. The DI framework provides an instance of this service at runtime.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -44,9 +52,31 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
     options.SlidingExpiration = true;
 });
+// These services are resolved from DI using constructor injection. The DI framework provides an instance of this service at runtime.
+// Constructor injection seems to be contained in all 'public class IndexModel : PageModel' -> index.cshtml.cs files
 
+// HOST
+// (3) -> .NET WebApplication Host, also known as the Minimal Host.
+// The .NET WebApplication Host is recommended and used in all the ASP.NET Core templates.
 var app = builder.Build();
+// The WebApplicationBuilder.Build method configures a host with a set of default options, such as:
+// 1) Use Kestrel as the web server and enable IIS integration.
+// 2) Load configuration from appsettings.json, environment variables, command line arguments, and other configuration sources.
+// 3) Send logging output to the console and debug providers.
+// --- Kestrel is a cross-platform web server. Kestrel is often run in a reverse proxy configuration using IIS. In ASP.NET Core 2.0 or later, Kestrel can be run as a public-facing edge server exposed directly to the Internet.
+// --- The server surfaces requests to the app as a set of request features composed into an HttpContext.
 
+// -------------
+// CONFIGURATION
+// -------------
+// Built-in configuration providers are available for a variety of sources, such as .json files, .xml files, environment variables, and command-line arguments.
+// By default, ASP.NET Core apps are configured to read from appsettings.json, environment variables, the command line, and more.
+// When the app's configuration is loaded, values from environment variables override values from appsettings.json.
+
+// ENVIRONMENT
+// Specify the environment an app is running in by setting the ASPNETCORE_ENVIRONMENT environment variable.
+// ASP.NET Core reads that environment variable at app startup and stores the value in an IWebHostEnvironment implementation.
+// This implementation is available anywhere in an app via dependency injection (DI).
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -54,11 +84,19 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
+    // Configures the exception handler and HTTP Strict Transport Security Protocol (HSTS) middleware when not running in the Development environment.
+    // Informs browsers that the site should only be accessed using HTTPS, and that any future attempts to access it using HTTP should automatically be converted to HTTPS.
+    // HTTPS uses TLS (SSL) to encrypt normal HTTP requests and responses, and to digitally sign those requests and responses.
     app.UseHsts();
 }
 
+// Middleware
+// Each component performs operations on an HttpContext and either invokes the next middleware in the pipeline or terminates the request.
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+// ROUTING
+// URL pattern that is mapped to a handler.
 
 app.UseRouting();
 
@@ -67,4 +105,21 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+
 app.Run();
+
+// ------------
+// Content Root
+// ------------
+// Razor files (.cshtml, .razor)
+// Configuration files (.json, .xml)
+// Data files (.db)
+
+// --------
+// Web Root
+// --------
+// In the wwwroot folder, base path for public, static resource files
+// Stylesheets (.css)
+// JavaScript (.js)
+// Images (.png, .jpg)
+
