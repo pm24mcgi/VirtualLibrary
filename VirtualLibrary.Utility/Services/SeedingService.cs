32b3d71f0df3764baa5ad5ee36b3d9f0d3669604
pm24mcgi <patrick.mcginn@troyweb.com>
2022-09-21 14:46:36 -0400

    Trying to get seed to work.

--- VirtualLibrary.Utility/Services/SeedingService.cs
+++ VirtualLibrary.Utility/Services/SeedingService.cs
@@ -1,4 +1,5 @@
 ﻿using VL.Data;
+using VL.Models;
 using VL.Utility.Interfaces;
 
 
@@ -14,7 +15,26 @@ namespace VL.Utility.Services
 
         public async Task SeedBooksAsync(int count)
         {
-            throw new NotImplementedException();
+            // I want to see if any books are inside of the databse on startup
+            var bookCheck = _applicationDbContext.Book;
+            // If there are no books
+            if (bookCheck == null)
+            {
+                return;
+                //// Call SeedBooksAsync
+                //await SeedBooksAsync(
+                //    // Key into the DB
+                //    // Call Add
+                //    // Create a new book instance
+                //    ApplicationDbContext.Add(new Book { Id = 1, Title = "Book1", Author = "Author1", ReleaseDate = new DateTime(2008, 6, 1, 0, 0, 0), CheckedOut = false, Description = "This is a lovely book." });
+
+                //// Save the cahnges to the DB
+                //ApplicationDbContext.SaveChanges();
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
