f7a7a49672c5e07995660a342d517d2c4ecfc53b
pm24mcgi <patrick.mcginn@troyweb.com>
2022-09-23 13:20:18 -0400

    Removing checkout model - not needed

--- VL.Models/Model/Checkout.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VL.Models
-{
-    public class Checkout
-    {
-        public int Id { get; set; }
-        public int BookId { get; set; }
-        public string UserId { get; set; }
-    }
-}
