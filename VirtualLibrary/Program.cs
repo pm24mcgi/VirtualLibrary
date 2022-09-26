c3d639a38a1812e18c4dcbb2cec3173117f291c2
pm24mcgi <patrick.mcginn@troyweb.com>
2022-09-26 18:39:01 -0400

    Removed fake email sender

--- VirtualLibrary/Program.cs
+++ VirtualLibrary/Program.cs
@@ -18,13 +18,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
+
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Identity/Account/Login";
     options.LogoutPath = "/Identity/Account/Logout";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
 });
-builder.Services.AddSingleton<IEmailSender, EmailSender>();
+
 builder.Services.AddScoped<ISeedingService, SeedingService>();
 
 var app = builder.Build();
