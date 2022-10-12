2a5d854f6da3f010a0b2f2e32098ea54e94bfa08
pm24mcgi <patrick.mcginn@troyweb.com>
2022-10-12 16:00:14 -0400

    JWT2

--- API/Controllers/BooksController.cs
+++ API/Controllers/BooksController.cs
@@ -22,7 +22,7 @@ namespace API.Controllers
         /// GET: api/Books
         /// </summary>
         /// <returns></returns>
-        [HttpGet]
+        [HttpGet, Authorize]
         [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         [ProducesDefaultResponseType]
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,11 @@
-using API.Config;
 using API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System.Text;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 using VL.Shared.Data;
 using VL.Shared.Interfaces;
 using VL.Shared.Services;
@@ -19,15 +19,42 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
 
-
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IMigrateService, MigrateService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<ISeedingService, SeedingService>();
+builder.Services.AddScoped<IBookService, BookService>();
 
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "JWT Auth header using Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+    var info = new OpenApiInfo()
+    {
+        Version = "1",
+        Title = "API",
+        Description = "Virtual Library API",
+    };
+
+    options.SwaggerDoc("v1", info);
+});
+
+// Add authentication scheme
 builder.Services
-    .AddAuthentication("Bearer")
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
         options.TokenValidationParameters = new TokenValidationParameters
@@ -38,48 +65,10 @@ builder.Services
             ValidIssuer = builder.Configuration["Authentication:Issuer"],
             ValidAudience = builder.Configuration["Authentication:Audience"],
             IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.ASCII.GetBytes(
-                    builder.Configuration["Authentication:SecretKey"]))
+                Encoding.UTF8
+                    .GetBytes(builder.Configuration["Authentication:SecretKey"]))
         };
     });
-
-builder.Services.AddScoped<IMigrateService, MigrateService>();
-builder.Services.AddScoped<IAccountService, AccountService>();
-builder.Services.AddScoped<ISeedingService, SeedingService>();
-builder.Services.AddScoped<IBookService, BookService>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SupportNonNullableReferenceTypes();
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Virtual Library API", Version = "v1" });
-    // Telling Swagger the type of protection wanted for the API
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Auth header using Bearer scheme.",
-        Name = "Auth",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT"
-    });
-
-    // We could add a global security requirement like this, makes no sense for this project
-    //options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    //{
-    //    {
-    //        new OpenApiSecurityScheme
-    //        {
-    //            Reference = new OpenApiReference
-    //            {
-    //                Type=ReferenceType.SecurityScheme,
-    //                Id="Bearer"
-    //            }
-    //        },
-    //        new string[]{}
-    //    }
-    //});
-});
 
 var app = builder.Build();
 
@@ -88,14 +77,16 @@ await app.MigrateDatabasesAsync();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    await app.SeedDataAsync();
     app.UseSwagger();
     app.UseSwaggerUI();
+    await app.SeedDataAsync();
 }
 
 app.UseHttpsRedirection();
 
+// Authorization Middleware
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
